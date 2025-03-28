524b773c1c2e18280ca2e1bf022721af3e8b21de
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-03-27 21:28:34 -0300

    Configurando Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      title="Memories API",
+ *      version="2.0.0",
+ *      description="Documentação da API usando Swagger"
+ * )
+ * @OA\Server(
+ *      url="http://127.0.0.1:8005",
+ *      description="Servidor de Produção"
+ * )
+ */
+
+
 abstract class Controller
 {
     //
